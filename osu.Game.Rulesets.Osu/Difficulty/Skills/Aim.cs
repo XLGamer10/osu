0b3c30bb0edd998ed9328a4c73b29d5cf7566abb
XLGamer10 <55kupczak@gmail.com>
2026-03-11 00:02:23 +0100

    make flow not completely stonks

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -31,9 +31,9 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private double currentStrain;
 
-        private double skillMultiplierSnap => 306.5;
-        private double skillMultiplierAgility => 12.1;
-        private double skillMultiplierFlow => 1231.6;
+        private double skillMultiplierSnap => 309.0;
+        private double skillMultiplierAgility => 12.9;
+        private double skillMultiplierFlow => 11070.6;
         private double skillMultiplierTotal => 1.1;
         private double meanExponent => 1.2;
 
