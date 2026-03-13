343b454a00f1ed413d779dfef6ce0553f62688a0
XLGamer10 <55kupczak@gmail.com>
2026-03-13 17:15:47 +0100

    fix a bracket after merging in rhythm

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -141,15 +141,17 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         }
 
         public double CountTopWeightedSliders(double difficultyValue)
-
+        {
             if (sliderStrains.Count == 0)
                 return 0;
+
             double consistentTopStrain = difficultyValue / 10; // What would the top strain be if all strain values were identical
             if (consistentTopStrain == 0)
                 return 0;
             // Use a weighted sum of all strains. Constants are arbitrary and give nice values
             return sliderStrains.Sum(s => DifficultyCalculationUtils.Logistic(s / consistentTopStrain, 0.88, 10, 1.1));
         }
+
         public double GetInaccuraciesWithCheesing() => maxStrain > 0 ? inaccuraciesWhileCheesing / maxStrain : 0;
 
         // Check if cheesing the current object still results in a great.
