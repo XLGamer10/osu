6f3e5984d3fb607cf93bbe3fa6e439430ecd4b9d
XLGamer10 <55kupczak@gmail.com>
2026-03-30 02:54:10 +0200

    cooking harder

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -16,7 +16,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double reading_window_size = 3000; // 3 seconds
         private const double distance_influence_threshold = OsuDifficultyHitObject.NORMALISED_DIAMETER * 1.5; // 1.5 circles distance between centers
         private const double minimum_distance_buff = OsuDifficultyHitObject.NORMALISED_DIAMETER * 5; // start buffing jumps for hidden from a certain distance onward
-        private const double hidden_distance_buff = 200;
+        private const double hidden_distance_buff = 600;
         private const double hidden_multiplier = 0.28;
         private const double density_multiplier = 2.4;
         private const double density_difficulty_base = 2.5;
@@ -128,13 +128,15 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
             // Account for both past and current densities
             double densityFactor = Math.Pow(currentVisibleObjectDensity + pastObjectDifficultyInfluence, 3.3) * 3;
 
-            double transparencyFactor = densityFactor;
-            double densityInfluence = DifficultyCalculationUtils.Smootherstep(transparencyFactor, 40, 300) * DifficultyCalculationUtils.Smootherstep(transparencyFactor, 10000, 2000);
+            double transparencyFactor = densityFactor + preemptFactor * 0.01;
+            double densityInfluence = DifficultyCalculationUtils.Smootherstep(transparencyFactor, 90, 400)
+                                      * (1 + DifficultyCalculationUtils.Smootherstep(transparencyFactor, 245, 2000) * 0.3)
+                                      * DifficultyCalculationUtils.Smootherstep(transparencyFactor, 15000, 3000);
 
-            double buffedDistance = Math.Pow(Math.Max(currObj.LazyJumpDistance, minimum_distance_buff) - minimum_distance_buff, 1.3);
+            double buffedDistance = Math.Pow(Math.Max(currObj.LazyJumpDistance, minimum_distance_buff) - minimum_distance_buff, 1.2);
             double distanceFactor = (buffedDistance - Math.Atan(buffedDistance)) * hidden_distance_buff * densityInfluence;
 
-            double hiddenDifficulty = (preemptFactor + densityFactor + distanceFactor) * constantAngleNerfFactor * velocity * 0.01;
+            double hiddenDifficulty = ((preemptFactor + densityFactor) * Math.Sqrt(constantAngleNerfFactor) + distanceFactor) * Math.Sqrt(constantAngleNerfFactor) * velocity * 0.01;
 
             // Apply a soft cap to general HD reading to account for partial memorization
             hiddenDifficulty = Math.Pow(hiddenDifficulty, 0.4) * hidden_multiplier;
