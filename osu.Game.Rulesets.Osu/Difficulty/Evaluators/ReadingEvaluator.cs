903fd300a4578770efacf24dfceafe9ac2fa3686
XLGamer10 <55kupczak@gmail.com>
2026-03-31 17:07:29 +0200

    make ghost rule hd not +50

--- osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Evaluators/ReadingEvaluator.cs
@@ -16,7 +16,8 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
         private const double reading_window_size = 3000; // 3 seconds
         private const double distance_influence_threshold = OsuDifficultyHitObject.NORMALISED_DIAMETER * 1.5; // 1.5 circles distance between centers
         private const double minimum_distance_buff = OsuDifficultyHitObject.NORMALISED_DIAMETER * 5; // start buffing jumps for hidden from a certain distance onward
-        private const double hidden_distance_buff = 40;
+        private const double hidden_very_long_distance_nerf = 10; // the higher the number, the less impact high distances have on the buff, pair an increase with a decrease in hidden_distance_buff
+        private const double hidden_distance_buff = 14;
         private const double hidden_multiplier = 0.28;
         private const double density_multiplier = 2.4;
         private const double density_difficulty_base = 2.5;
@@ -134,14 +135,14 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Evaluators
                                       * (1 + DifficultyCalculationUtils.Smootherstep(transparencyFactor, 245, 2000) * 0.3)
                                       * DifficultyCalculationUtils.Smootherstep(transparencyFactor, 15000, 3000);
 
-            double buffedDistance = Math.Pow(Math.Max(currObj.LazyJumpDistance, minimum_distance_buff) - minimum_distance_buff, 1.0);
+            double buffedDistance = Math.Max(currObj.LazyJumpDistance, minimum_distance_buff) - minimum_distance_buff;
             double distanceFactor = (buffedDistance - Math.Atan(buffedDistance)) * densityInfluence;
 
             double inherentHiddenDifficulty = (preemptFactor + densityFactor) * constantAngleNerfFactor * velocity * 0.01;
-            double distanceHiddenDifficulty = Math.Log(distanceFactor * Math.Sqrt(constantAngleNerfFactor * velocity) * 0.01 + 1) * hidden_distance_buff;
+            double distanceHiddenDifficulty = Math.Log(hidden_very_long_distance_nerf * distanceFactor * Math.Sqrt(constantAngleNerfFactor * velocity) * 0.01 + 1) * hidden_distance_buff;
 
             // Apply a soft cap to general HD reading to account for partial memorization
-            double hiddenDifficulty = (Math.Pow(inherentHiddenDifficulty, 0.4) + Math.Pow(distanceHiddenDifficulty, 0.75)) * hidden_multiplier;
+            double hiddenDifficulty = (Math.Pow(inherentHiddenDifficulty, 0.4) + Math.Pow(distanceHiddenDifficulty, 0.6)) * hidden_multiplier;
 
             var previousObj = (OsuDifficultyHitObject)currObj.Previous(0);
 
