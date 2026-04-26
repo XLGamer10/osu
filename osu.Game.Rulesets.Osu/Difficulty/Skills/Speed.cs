824acd24ae95776ca7c5d8e414c89f16e5c7ad70
XLGamer10 <55kupczak@gmail.com>
2026-04-27 00:50:06 +0200

    .

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Speed.cs
@@ -22,10 +22,16 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
         private double skillMultiplier => 1.16;
 
         private readonly List<double> sliderStrains = new List<double>();
+        private readonly List<double> deltaTimesList = new List<double>();
+        private readonly List<double> startTimesList = new List<double>();
 
         private double currentDifficulty;
 
         private double strainDecayBase => 0.3;
+        private double maxDeltaTime => 5000;
+        private double timeWeightSize => 200;
+        private double startTimeInfluence => 500000;
+        private double weightExponent => 0.0;
 
         protected override double HarmonicScale => 20;
         protected override double DecayExponent => 0.9;
@@ -50,6 +56,11 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
             if (current.BaseObject is Slider)
                 sliderStrains.Add(totalDifficulty);
+
+            if (totalDifficulty <= 0) return totalDifficulty;
+
+            deltaTimesList.Add(Math.Min(((OsuDifficultyHitObject)current).AdjustedDeltaTime, maxDeltaTime));
+            startTimesList.Add(current.StartTime);
 
             return totalDifficulty;
         }
@@ -82,6 +93,62 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
             // Use a weighted sum of all notes. Constants are arbitrary and give nice values
             return sliderStrains.Sum(s => DifficultyCalculationUtils.Logistic(s / consistentTopNote, 0.88, 10, 1.1));
+        }
+
+        public override double DifficultyValue()
+        {
+            if (ObjectDifficulties.Count == 0)
+                return 0;
+
+            // Notes with 0 difficulty are excluded to avoid worst-case time complexity of the following sort (e.g. /b/2351871).
+            // These notes will not contribute to the difficulty.
+            double[] difficulties = ObjectDifficulties.Where(p => p > 0).ToArray();
+            double[] difficultiesCopy = difficulties; // Created because .Sort() only accepts one extra array to sort while we need to sort two
+            double[] deltaTimes = deltaTimesList.ToArray();
+            double[] startTimes = startTimesList.ToArray();
+
+            if (difficulties.Length == 0)
+                return 0;
+
+            ApplyDifficultyTransformation(difficulties);
+
+            Array.Sort(difficulties, deltaTimes); // Sorts the difficulties and deltaTimes arrays according to difficulties
+            Array.Sort(difficultiesCopy, startTimes); // Sorts startTimes in the same way as the above
+            Array.Reverse(difficulties); // Descending order
+            Array.Reverse(deltaTimes);
+            Array.Reverse(startTimes);
+
+            double difficulty = 0;
+            double time = 0;
+            int index = 0;
+
+            foreach (double note in difficulties)
+            {
+                // Use a harmonic sum that considers each note of the map according to a predefined weight.
+                double weight = (1 + HarmonicScale / (1 + time)) / (Math.Pow(time, DecayExponent) + 1 + HarmonicScale / (1 + time))
+                                * deltaTimes[index] / timeWeightSize // To ensure that multiple fast notes are weighted the same as a slow note
+                                * Math.Log(startTimes[index] + startTimeInfluence, startTimeInfluence); // Buff difficult notes later on in the map
+
+                NoteWeightSum += weight;
+                difficulty += note * weight;
+                time += deltaTimes[index] / timeWeightSize;
+                index += 1;
+            }
+
+            return difficulty;
+        }
+
+        protected override void ApplyDifficultyTransformation(double[] difficulties)
+        {
+            if (weightExponent <= 0) return; // just in case someone puts in a negative number
+
+            double peakDifficulty = difficulties.Max();
+
+            // Reduce the difficulty of notes that are easier than the most difficult object
+            for (int i = 0; i < difficulties.Length; i++)
+            {
+                difficulties[i] *= Math.Pow(difficulties[i], weightExponent) / Math.Pow(peakDifficulty, weightExponent);
+            }
         }
     }
 }
